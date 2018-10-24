2db5166aec394aac302a5da8cf249a30f7569957
reafle <reafle@gmail.com>
2018-10-25 02:00:00 +0300

    Some test events

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ const
 ;
 
 io.on('connect', (socket) => {
-  socket.on('room', (request) => {
-    socket.join(ROOM);
-    console.log('joined: ');
-    console.log(request);
+  console.log('connected');
+  socket.emit('USER_JOINED', { name: 'test user' });
+  socket.emit('MESSAGE_RECEIVED', { user: 'test user', text: ' hello from the backend' });
+
+  socket.on('users', () => {
+    console.log('requesting USERS: ');
+  });
+
+  socket.on('message', (user, message) => {
+    console.log('MESSAGE from ' + user + ' : ' + 'message');
+  });
+
+  socket.on('ping', () => {
+    console.log('PING got: ');
+    socket.emit('pong');
   });
 });
 
